cb5833c72a818e24bf884dd942699f347ed80990
faraznaeem <faraznaeem@gmail.com>
2020-03-14 12:09:16 +0100

    Modifies request spec

--- spec/requests/api/client_can_create_new_order_spec.rb
+++ spec/requests/api/client_can_create_new_order_spec.rb
@@ -1,10 +1,20 @@
 RSpec.describe Api::OrdersController, type: :request do
-	let!(:product_1) {  create(:product, name: 'Pizza')  }
-	let!(:product_2) {  create(:product, name: 'Kebab')  }
+  let!(:product_1) { create(:product, name: "Pizza") }
+  let!(:product_2) { create(:product, name: "Kebab") }
 
-	it 'responds with success message' do
-		post '/api/orders', params: {id: product_1.id }
+  before do
+    post "/api/orders", params: { id: product_1.id }
+    @order = Order.last
+  end
 
-		expect(JSON.parse(response.body)['message']).to eq 'The product has been added to your order'
-	end
+  it 'adds another product to order if param order_id"
+     is present' do
+    post "/api/orders", params: { id: product_2.id, order_id: @order.id }
+    expect(@order.order_items.count).to eq 2
+  end
+
+  it "responds with success message" do
+    post "/api/orders", params: { id: product_1.id }
+    expect(JSON.parse(response.body)["message"]).to eq "The product has been added to your order"
+  end
 end
