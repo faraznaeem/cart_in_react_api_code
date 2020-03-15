51042e2b05f3d97f19eee4a8791c70e0984da5d8
faraznaeem <faraznaeem@gmail.com>
2020-03-15 15:33:59 +0100

    Creates new request spec for finalized order

---
+++ spec/requests/api/user_can_finalize_order_spec.rb
@@ -0,0 +1,22 @@
+RSpec.describe Api::OrdersController, type: :request do
+  let(:product_1) { create(:product, name: 'Pizza', price: 10) }
+  let(:product_2) { create(:product, name: 'Kebab', price: 20) }
+  let(:order) { create(:order) }
+
+  before do
+    order.order_items.create(product: product_1)
+    order.order_items.create(product: product_2)
+
+    put "/api/orders/#{order.id}", params: { action: 'finalize' }
+  end
+
+  describe 'PUT /api/orders' do
+    it 'responds with success message' do
+      expect(JSON.parse(response.body)['message']).to eq 'Your order will be ready in 30 minutes!'
+		end
+
+		it 'sets the order attribute "finalized" to true' do
+			expect(order.reload.finalized).to eq true
+		end
+  end
+end
